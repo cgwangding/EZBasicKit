e1b4f62b36037bb8b63d8853ac549ff030356cb5
许赟 <xuyun@ezbuy.com>
2019-05-29 14:44:54 +0800

    add SelectionTests file

---
+++ Example/Tests/SelectionTests.swift
@@ -0,0 +1,19 @@
+//
+//  SelectionTests.swift
+//  EZBasicKit_Tests
+//
+//  Created by ezbuy on 2019/5/29.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import EZBasicKit
+
+class SelectionTests: QuickSpec {
+
+    override func spec() {
+        
+    }
+
+}
