b241cf62985797b6ead8c3df59b8152dcc64df16
许赟 <xuyun@ezbuy.com>
2019-05-29 17:42:59 +0800

    create selectionTests

--- Example/Tests/SelectionTests.swift
+++ Example/Tests/SelectionTests.swift
@@ -14,6 +14,23 @@ class SelectionTests: QuickSpec {
 
     override func spec() {
         
+        describe("a Customer") {
+            let customer = Customer()
+            let apple = Fruit(count: 4, type: .🍎)
+            let banana = Fruit(count: 6, type: .🍌)
+            let pineapple = Fruit(count: 1, type: .🍍)
+            customer.objects = [apple, banana, pineapple]
+            var selection: SetSelection<Customer>!
+            beforeEach {
+                selection = SetSelection<Customer>()
+                selection.source = customer
+            }
+            
+            describe("select", {
+                expect(selection.setSelected(true, forObject: apple)).to(equal(true))
+                
+            })
+        }
     }
 
 }
