8f4cf93194101b89c8fb04227f1978b8b0483c87
wangding <wangding@ezbuy.com>
2019-06-05 13:57:57 +0800

    Add animator vc

---
+++ Example/EZBasicKit/AnimatorDemo/AnimatorDemoTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AnimatorDemoTableViewController.swift
+//  EZBasicKit_Example
+//
+//  Created by wangding on 2019/6/5.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class AnimatorDemoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
