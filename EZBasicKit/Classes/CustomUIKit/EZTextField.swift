abdd9c81d1e5ed3a1cf47223e86dd0ddf9aca661
wangding <wangding@ezbuy.com>
2019-06-06 17:03:21 +0800

    Add custom uikit demo

--- EZBasicKit/Classes/CustomUIKit/EZTextField.swift
+++ EZBasicKit/Classes/CustomUIKit/EZTextField.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 public class EZTextField : UITextField {
 
     @IBInspectable public var borderColor : UIColor? {
--- EZBasicKit/Classes/CustomUIKit/StrokeBorderView.swift
+++ EZBasicKit/Classes/CustomUIKit/StrokeBorderView.swift
@@ -11,11 +11,23 @@ import UIKit
 @IBDesignable
 open class StrokeBorderView: UIView {
 
+    public enum RectEdge: Int {
+        case left = 0
+        case right
+        case bottom
+        case top
+        case all
+    }
+
     @IBInspectable
     open var lineWidth: CGFloat = 1.0
 
     @IBInspectable
     open var lineColor: UIColor?
+
+    open var rectEdge: Set<RectEdge> = [.all]
+
+    open var lineDashPattern: [NSNumber] = [6, 2]
 
     lazy var borderLayer: CAShapeLayer = {
 
@@ -23,12 +35,53 @@ open class StrokeBorderView: UIView {
         shapeLayer.strokeColor = self.lineColor?.cgColor
         shapeLayer.fillColor = nil
         shapeLayer.lineWidth = self.lineWidth
-        shapeLayer.lineDashPattern = [6, 2]
-
+        shapeLayer.lineDashPattern = self.lineDashPattern
         self.layer.addSublayer(shapeLayer)
         return shapeLayer
 
     }()
+
+    var strokePath: UIBezierPath {
+
+        let p = UIBezierPath()
+
+        let halfWidth: CGFloat = self.lineWidth / 2.0
+
+        if self.rectEdge.contains(.all) {
+            p.move(to: CGPoint(x: bounds.origin.x, y: bounds.origin.y + halfWidth))
+            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.origin.y + halfWidth))
+            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.size.height - halfWidth))
+            p.addLine(to: CGPoint(x: bounds.origin.x + halfWidth, y: bounds.size.height - halfWidth))
+            p.addLine(to: CGPoint(x: bounds.origin.x + halfWidth, y: bounds.origin.y))
+            debugPrint("all")
+        }
+
+        if self.rectEdge.contains(.left) {
+            p.move(to: CGPoint(x: bounds.origin.x, y: bounds.origin.y + halfWidth))
+            p.addLine(to: CGPoint(x: bounds.origin.x, y: bounds.size.height))
+            debugPrint("left")
+        }
+
+        if self.rectEdge.contains(.top) {
+            p.move(to: CGPoint(x: bounds.origin.x, y: bounds.origin.y + halfWidth))
+            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.origin.y + halfWidth))
+            debugPrint("top")
+        }
+
+        if self.rectEdge.contains(.right) {
+            p.move(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.origin.y + halfWidth))
+            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.size.height - halfWidth))
+            debugPrint("right")
+        }
+
+        if self.rectEdge.contains(.bottom) {
+            p.move(to: CGPoint(x: bounds.origin.x, y: bounds.size.height - halfWidth))
+            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.size.height - halfWidth))
+            debugPrint("bottom")
+        }
+
+        return p
+    }
 
     override open func layoutSubviews() {
         super.layoutSubviews()
@@ -36,23 +89,9 @@ open class StrokeBorderView: UIView {
         let layer = self.layer
         let bounds = self.bounds
 
-        let path: UIBezierPath = {
-
-            let p = UIBezierPath()
-
-            let halfWidth: CGFloat = self.lineWidth / 2.0
-
-            p.move(to: CGPoint(x: bounds.origin.x, y: bounds.origin.y + halfWidth))
-            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.origin.y + halfWidth))
-            p.addLine(to: CGPoint(x: bounds.size.width - halfWidth, y: bounds.size.height - halfWidth))
-            p.addLine(to: CGPoint(x: bounds.origin.x, y: bounds.size.height - halfWidth))
-
-            return p
-        }()
-
         let borderLayer = self.borderLayer
         borderLayer.frame = layer.bounds
-        borderLayer.path = path.cgPath
+        borderLayer.path = self.strokePath.cgPath
 
     }
 }
@@ -73,7 +112,7 @@ open class DottedLineView: UIView {
         shapeLayer.fillColor = nil
         shapeLayer.lineWidth = self.lineWidth
         shapeLayer.lineDashPattern = [6, 2]
-        
+
         self.layer.addSublayer(shapeLayer)
         return shapeLayer
         
