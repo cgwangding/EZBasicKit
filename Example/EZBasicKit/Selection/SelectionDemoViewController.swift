0e0693b0ca2218013e046ab52c0208f1691c20c9
许赟 <xuyun@ezbuy.com>
2019-06-18 17:24:25 +0800

    configure SetSelectionDemo UI

--- Example/EZBasicKit/Selection/SelectionDemoViewController.swift
+++ Example/EZBasicKit/Selection/SelectionDemoViewController.swift
@@ -10,21 +10,59 @@ import UIKit
 
 class SelectionDemoViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var controller: ProductController = ProductController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        controller.loadData()
     }
-    */
 
+    @IBAction func nextBtnDidTapped(_ sender: UIButton) {
+        let selectedObjects = controller.selection.selectedObjects
+        guard !selectedObjects.isEmpty else {
+            self.presentAlert(title: "", message: "Please select one before click next", buttontitle: "OK")
+            return
+        }
+        selectedObjects.forEach { (obj) in
+            print(obj.identifier)
+        }
+    }
+}
+
+extension SelectionDemoViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return controller.objects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell", for: indexPath)
+        
+        if let obj = controller.objects.object(at: indexPath.row) {
+            let title = cell.viewWithTag(200) as? UILabel
+            title?.text = "\(obj.identifier)"
+            let btn = cell.viewWithTag(201) as? UIButton
+            btn?.isSelected = controller.selection.isSelected(obj)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let obj = controller.objects.object(at: indexPath.row) {
+            let isSelected = controller.selection.isSelected(obj)
+            controller.selection.setSelected(!isSelected, forObject: obj)
+            tableView.reloadData()
+        }
+    }
 }
