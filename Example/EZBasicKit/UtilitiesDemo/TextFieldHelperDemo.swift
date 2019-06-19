3171671ff3e1c3450754a8b5146b384c38e40d70
许赟 <xuyun@ezbuy.com>
2019-06-19 14:52:01 +0800

    add TextFieldHelperDemo

--- Example/EZBasicKit/UtilitiesDemo/TextFieldHelperDemo.swift
+++ Example/EZBasicKit/UtilitiesDemo/TextFieldHelperDemo.swift
@@ -9,51 +9,22 @@
 import UIKit
 import EZBasicKit
 
-class TextFieldHelperView: UIView, UITextFieldDelegate {
-
-    lazy var textField: UITextField = {
-        let field = UITextField(frame: self.bounds)
-        field.delegate = self
-        return field
-    }()
+class TextFieldHelperViewController: UIViewController, UITextFieldDelegate {
     
-    var inputValueItem: String? {
-        didSet {
-            self.updateCellWithCurrentItem()
-        }
-    }
+    @IBOutlet weak var textField: UITextField!
     
-    let chinaPhoneNumberHelper = PhoneNumberTextFieldHelper(lengthLimit: 11, decorators: nil, tolerancePrefix: "86")
+    var inputValueItem: String?
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        //add editing selector to textField in the init method of textField's superview or drag an action from xib or storybard
+    let chinaPhoneNumberHelper = PhoneNumberTextFieldHelper(lengthLimit: 11, decorators: nil, tolerancePrefix: "+86")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         textField.addTarget(self, action: #selector(textFieldValueDidChange(sender:)), for: .editingChanged)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        textField.addTarget(self, action: #selector(textFieldValueDidChange(sender:)), for: .editingChanged)
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        textField.addTarget(self, action: #selector(textFieldValueDidChange(sender:)), for: .editingChanged)
-    }
-    
-    func updateCellWithCurrentItem() {
-        if let item = self.inputValueItem {
-            self.textField.text = item
-        } else {
-            self.textField.text = nil
-        }
+        textField.delegate = chinaPhoneNumberHelper
     }
     
     @objc func textFieldValueDidChange(sender: UITextField) {
-        self.inputValueItem = self.chinaPhoneNumberHelper.adjustedTextFromTextField(textField)
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        return self.chinaPhoneNumberHelper.textField(textField, shouldChangeCharactersIn: range, replacementString: string) 
+        self.inputValueItem = self.chinaPhoneNumberHelper.adjustedTextFromTextField(sender)
+        print(self.inputValueItem ?? "")
     }
 }
