f56acbda1dd198b39613c3b1e87f1f7226725981
许赟 <xuyun@ezbuy.com>
2019-06-21 15:44:34 +0800

    configure ObjectsControllerBaseDemoViewController

--- Example/EZBasicKit/ObjectsControllerDemo/ObjectsControllerBaseDemo.swift
+++ Example/EZBasicKit/ObjectsControllerDemo/ObjectsControllerBaseDemo.swift
@@ -7,24 +7,86 @@
 //
 
 import UIKit
+import EZBasicKit
 
 class ObjectsControllerBaseDemoViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate let controller = CreditCardController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func loadData() {
+        
+        let processing = controller.reload(completion: { (_) in
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.activityIndicator.stopAnimating()
+                self.tableView.reloadData()
+            }
+            
+        }, failure: { (error) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.activityIndicator.stopAnimating()
+            }
+        })
+        
+        if processing {
+            self.activityIndicator.isHidden = false
+            self.activityIndicator.startAnimating()
+        }
     }
-    */
+}
 
+extension ObjectsControllerBaseDemoViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return controller.objects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let card = controller.objects.object(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CreditCardInfo", for: indexPath)
+        
+        if let cardNum = card?.cardNum.dropLast(4) {
+            cell.textLabel?.text = "**** " + cardNum
+        }
+        
+        return cell
+    }
+}
+
+class CreditCardController: ObjectsControllerBase<CreditCard> {
+    
+    override func reload(completion: @escaping ([CreditCard]) -> Void, failure: @escaping (Error) -> Void) -> Bool {
+        // request server to load credit card data
+        
+        let result: Bool = true //request result
+        
+        if result {
+            let card1 = CreditCard(num: "4242424242424242")
+            let card2 = CreditCard(num: "4444000000000000")
+            let card3 = CreditCard(num: "5555555555556666")
+            self.objects = [card1, card2, card3]
+            completion(self.objects)
+        } else {
+            //failure(Error)
+        }
+        
+        return true
+    }
 }
