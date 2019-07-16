9cd0e33360ac071b7f4c8417376e8ae92739ffd1
许赟 <xuyun@ezbuy.com>
2019-07-16 16:06:08 +0800

    add OneRequestObjectsControllerDemo

---
+++ Example/EZBasicKit/ObjectsControllerDemo/OneRequestObjectsControllerDemo.swift
@@ -0,0 +1,17 @@
+//
+//  OneRequestObjectsControllerDemo.swift
+//  EZBasicKit_Example
+//
+//  Created by ezbuy on 2019/7/16.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class OneRequestObjectsControllerDemo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
